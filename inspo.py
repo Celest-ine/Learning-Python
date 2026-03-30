73e7f50abba35904d1ceebebf3a86dc8ad4b3d38
Celestine Wangechi <wangechicelestine@gmail.com>
2026-03-30 15:30:03 +0300

    Output a quote with authors name

---
+++ inspo.py
@@ -0,0 +1,14 @@
+# This ia a script that prints an inpirational message.
+
+myname = 'Celeste'
+print(f"Hey {myname}, I will soon make you proud")
+
+# prints name in lowercase
+print(myname.lower())
+
+# Prints name in uppercase
+print(myname.upper())
+
+# Prints name in title
+print(myname.title())
+
