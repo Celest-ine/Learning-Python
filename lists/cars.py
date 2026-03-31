2e1f3723dc5354c5dbb80ae89368bd9b4fce8c02
Celestine Wangechi <wangechicelestine@gmail.com>
2026-03-31 05:37:57 +0300

    Print items in a list with in a statement

---
+++ lists/cars.py
@@ -0,0 +1,6 @@
+# A list of desired cars
+cars = ['Kia sportage', 'Toyota Vitz', 'Ford Ranger']
+
+print(f'I have {cars[0]} as the first car in my collection.')
+print(f'I bought {cars[1]} as my test car')
+print(f'I plan to buy {cars[2]} when I have a farm')
