e2d39c70129ae316035880fc37ee871171531936
Celestine Wangechi <wangechicelestine@gmail.com>
2026-03-31 05:36:07 +0300

    Print items in a list one at a time

---
+++ lists/names.py
@@ -0,0 +1,16 @@
+# Store names in a list and access each name one at a time.
+
+names = ['Joy', 'Blue', 'Lily', 'Andrew', 'Paul']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+# print a greeting message for each item in the list.
+message = 'Hello there, '
+print(message + names[0])
+print(message + names[1])
+print(message + names[2])
+print(message + names[3])
+print(message + names[4])
