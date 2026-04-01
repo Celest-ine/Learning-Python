2d7f1e3f2553abe7cd1ce748c6c2b7b11d648ec8
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-01 10:24:39 +0300

    Sort, reverse and reverse sort s list

---
+++ lists/seeing_the_world.py
@@ -0,0 +1,14 @@
+# Seeing the world
+
+locations = ['maldives', 'bali', 'zanzibar', 'thailand', 'kigali']
+print('Original list:', locations)
+print('Sorted list:', sorted(locations))
+print('Original list after sorted:', locations)
+print('Reversed list:', sorted(locations, reverse=True))
+print('list after sorted with reverse:', locations)
+locations.reverse()
+print('List after reverse:', locations)
+locations.sort()
+print('list after sort:', locations)
+locations.sort(reverse=True)
+print('List after sort in reverse:', locations)
