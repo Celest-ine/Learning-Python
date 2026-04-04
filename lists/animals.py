1032cd41d74a0ce1bdaf5f187fb9bc2c61d6b0de
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-04 11:57:05 +0300

    Use a for loop to print a statement with list items

---
+++ lists/animals.py
@@ -0,0 +1,9 @@
+# A list of animals
+animals = ['dog', 'cow', 'goat']
+# print the list of animals
+for animal in animals:
+    print(animal.title())
+# Print a sentence for each animal in the list
+    print(f"A {animal} would make a great pet.\n")
+# Add a sentence at the end of the loop
+print("All these animals are mammals.")
