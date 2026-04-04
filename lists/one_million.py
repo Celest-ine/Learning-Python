05f4798ab839295f3997dab151c72bdf99d0d771
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-04 14:22:41 +0300

    Create a list of numbers from 1 to 1millon and use a for loop to print each number

---
+++ lists/one_million.py
@@ -0,0 +1,6 @@
+# Create a list of numbers from 1 to 1 million then use a for loop to print the numbers.
+# create the list
+numbers = list(range(1, 1000001))
+# print the numbers
+for number in numbers:
+    print(number)
