bce58e888addacf64a2b5edd32dcd64791a5250b
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-04 14:45:03 +0300

    Print the minimum, maximum and sum of numbers from 1 to 1million

---
+++ lists/summing_a_million.py
@@ -0,0 +1,6 @@
+# Sum a million numbers
+numbers = list(range(1, 1000001))
+
+print(f'The minimum number is: {min(numbers)}')
+print(f'The maximum number is: {max(numbers)}')
+print(f'The sum of the numbers is: {sum(numbers)}')
