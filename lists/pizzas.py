aa9d87fcbc7bae576f602ef4d963bb06b3b3a37e
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-04 11:17:02 +0300

    Use for loops in list

---
+++ lists/pizzas.py
@@ -0,0 +1,9 @@
+# A list that uses a for loop to print  a sentence for each item in the list.
+pizzas = ['beef barbeque', 'diavola', 'hawaiian', 'pepperoni', 'margherita']
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+# A sentence at the end of the for loop, outside the for loop.
+favorite_pizza = pizzas[0]
+print(f"\nMy favorite pizza of them all is {favorite_pizza} pizza.\nPizza makes me happy!\nI really love pizza!")
