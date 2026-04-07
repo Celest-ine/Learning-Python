e7beba49f6daf7e3e8d3583e827e1653c4e27cd5
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-07 18:23:03 +0300

    Run more than one if statement

---
+++ conditionals/favorite_fruits.py
@@ -0,0 +1,13 @@
+# Check if a fruit is in the favorite fruits list.
+favorite_fruits = ['apple', 'banana','cherry']
+
+if 'apple' in favorite_fruits:
+    print('You really like apples!')
+if 'pineapple' in favorite_fruits:
+    print('You really like pineapples!')
+if 'banana' in favorite_fruits:
+    print('You really like bananas!')
+if 'grape' in favorite_fruits:
+    print('You really like grapes!')
+if 'cherry' in favorite_fruits:
+    print('You really like cherries!')
