63cbb995c3282f8cdee4c60868c1f304600c81da
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-07 17:25:34 +0300

    A program that assigns you points depending on the color of alien

---
+++ conditionals/alien_colors.py
@@ -0,0 +1,13 @@
+# Assign a color to an alien and check if it is green.
+#alien_color = 'green'
+
+# Pass the test by changing the value of alien_color
+alien_color = 'red'
+#alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points.")
+elif alien_color == 'yellow':
+    print("Congratulations! You just earned 10 points.")
+else:
+    print("Congratulations! You just earned 15 points.")
