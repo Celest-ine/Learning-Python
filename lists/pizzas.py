bc653b070a05da96d76a162ce91c26f5fca376cb
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-07 12:09:19 +0300

    modify: Modify the program to use slice on the list

--- lists/pizzas.py
+++ lists/pizzas.py
@@ -1,6 +1,21 @@
 # A list that uses a for loop to print  a sentence for each item in the list.
 pizzas = ['beef barbeque', 'diavola', 'hawaiian', 'pepperoni', 'margherita']
 
+# Use slice to print the first three items in the list.
+print('The first three items in the list are:')
+for pizza in pizzas[:3]:
+    print(f'{pizza.title()} pizza')
+
+# Use slice to print two items from the middle of the list.
+print('\nTwo items from the middle of the list are:')
+for pizza in pizzas[3:5]:
+    print(f'{pizza.title()} pizza')
+
+# use slice to print the last three items in the list.
+print('\nThe 3 last items in the list are:')
+for pizza in pizzas[-3:]:
+    print(f'{pizza.title()} pizza')
+
 for pizza in pizzas:
     print(f"I like {pizza} pizza.")
 
