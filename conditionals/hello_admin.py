754a59f8b09a7f321c2a0f0af51f0dcc7296c76a
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-07 19:32:48 +0300

    A program that writes a special message for the admin and generic ones for the rest of thr users

---
+++ conditionals/hello_admin.py
@@ -0,0 +1,7 @@
+# Check if a user is an admin and greet them accondingly.
+users = ['admin', 'john', 'joy', 'sarah', 'james']
+for user in users:
+    if user == 'admin':
+        print(f'Hello {user}, would you like to see a status report?')
+    else:
+        print(f'Hello {user.title()}, thank you for logging in again.')
