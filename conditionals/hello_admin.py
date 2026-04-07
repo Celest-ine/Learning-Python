4cfe732e7d3760d2a9b71af72aec39384ab28160
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-07 19:41:39 +0300

    add an if statement to handle the event that the list is empty

--- conditionals/hello_admin.py
+++ conditionals/hello_admin.py
@@ -1,7 +1,12 @@
 # Check if a user is an admin and greet them accordingly.
-users = ['admin', 'john', 'joy', 'sarah', 'james']
-for user in users:
-    if user == 'admin':
-        print(f'Hello {user}, would you like to see a status report?')
-    else:
-        print(f'Hello {user.title()}, thank you for logging in again.')
+
+# users = ['admin', 'john', 'joy', 'sarah', 'james']
+users = []
+if users:
+    for user in users:
+        if user == 'admin':
+            print(f'Hello {user}, would you like to see a status report?')
+        else:
+            print(f'Hello {user.title()}, thank you for logging in again.')
+else:
+    print('We need to find some users!')
