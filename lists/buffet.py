1f0d4757ef5a8d354878d2b9a04a441de3e88837
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-07 13:44:26 +0300

    style: Missing space around the '=' sign in line 11 and spelling mistake fix on line 2

--- lists/buffet.py
+++ lists/buffet.py
@@ -1,12 +1,13 @@
-# A tuple of foods a buffet-style resturant offers.
-buffet = ('sushi', 'mashroom soup', 'T-bone steak', 'mushed potatoes', 'chocolate mousse')
+# A tuple of foods a buffet-style restaurant offers.
+buffet = ('sushi', 'mushroom soup', 'T-bone steak', 'mashed potatoes', 'chocolate mousse')
 for food in buffet:
     print(food)
-print('\n')
+print()
+
 # Try to change one of the items. This will cause an error because tuples are immutable.
 #buffet[1] = 'clam chowder'
 
-#Rewrite the tuple to replce some foods. The only way to change a tuple.
-buffet =('sushi', 'clam chowder', 'T-bone steak', 'baked potatoes', 'chocolate mousse')
+#Rewrite the tuple to replace some foods. The only way to change a tuple.
+buffet = ('sushi', 'clam chowder', 'T-bone steak', 'baked potatoes', 'chocolate mousse')
 for food in buffet:
     print(food)
