b2188b3d983129d27bb9712821c0bdee006c381e
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-08 16:01:39 +0300

    A dictionary that stores information about a person

---
+++ dictionaries/person.py
@@ -0,0 +1,11 @@
+# A dictionary with information about a person
+person = {
+    "first_name": "Celeste",
+    "last_name": "Wangechi",
+    "age": 25,
+    "city": "Nairobi",
+}
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
