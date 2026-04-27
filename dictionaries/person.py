3631ac7f0712cf952b5fd689d9f14687223a2ebc
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-27 10:53:14 +0300

    Create dictionaries, store them in a list and loop through the list to ouput data from the dictionaries

--- dictionaries/person.py
+++ dictionaries/person.py
@@ -1,11 +1,29 @@
 # A dictionary with information about a person
-person = {
-    "first_name": "Celeste",
-    "last_name": "Wangechi",
+# Modify the code to have three dictionaries and store all three dictionaries in a list.
+person1 = {
+    "first_name": "celeste",
+    "last_name": "wangechi",
     "age": 25,
-    "city": "Nairobi",
+    "city": "nairobi",
 }
-print(person["first_name"])
-print(person["last_name"])
-print(person["age"])
-print(person["city"])
+person2 = {
+    "first_name": "blue",
+    "last_name": "ren",
+    "age": 20,
+    "city": "paris",
+}
+
+person3 = {
+    "first_name": "paula",
+    "last_name": "green",
+    "age": 23,
+    "city": "florida"
+}
+people = [person1, person2, person3]
+
+# Loop through the list and print info about each person.
+for person in people:
+    print(f"Fullname: {person['first_name'].title()} {person['last_name'].title()}")
+    print(f"\tAge: {person['age']}")
+    print(f"\tCity: {person['city'].title()}")
+    print()
