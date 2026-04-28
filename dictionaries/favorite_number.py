a6de33dc0242d0a70ca77c5c3e6bb143ca1ef579
Celestine Wangechi <wangechicelestine@gmail.com>
2026-04-28 13:06:43 +0300

    Modify the dictionary so that each person has more than one favorite number

--- dictionaries/favorite_number.py
+++ dictionaries/favorite_number.py
@@ -1,11 +1,14 @@
 # A dictionary that maps names to their favorite numbers.
+# Modify for each person to have more than one number.
 favorite_numbers = {
-    "joy": 6,
-    "ivy": 9,
-    "bahati": 3,
-    "kim": 7,
-    "alex": 4,
+    "joy": [6, 8, 9],
+    "ivy": [9, 4, 2],
+    "bahati": [3, 1, 4, 5, 8],
+    "kim": [7, 2],
+    "alex": [4, 6, 3, 7],
 }
 # Print each person's name and their favorite number.
-for name, number in favorite_numbers.items():
-    print(f"{name.title()}'s favorite number is {number}.")
+for name, numbers in favorite_numbers.items():
+    print(f"{name.title()}'s favorite numbers are:")
+    for number in numbers:
+        print(f"\t{number}")
