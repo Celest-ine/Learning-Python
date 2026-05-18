3e2efcc905d5fd1f8d52ecdfb4a3eb35b7263cc1
Celestine Wangechi <wamgechicelestine.gmail.com>
2026-05-18 18:41:21 +0300

    A program that asks the user what kind of rental car they want

---
+++ whileLoops/rental_car.py
@@ -0,0 +1,8 @@
+# A program that asks the user what kind of rental car they would like and displays a message about the car.
+
+car = input("What kind of car would you like to rent? ").strip()
+
+if not car:
+    print("You need to enter a car model.")
+else:
+    print(f"Let me see if I can find you a {car.title()}.")
