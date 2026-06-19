68857d743003eb2e94ef45427bf9f617c1f934b6
Celestine Wangechi <wangechicelestine@gmail.com>
2026-06-19 13:17:55 +0300

    fix: few spelling mistakes in the comments and print statements

--- whileLoop/deli.py
+++ whileLoop/deli.py
@@ -1,15 +1,15 @@
-# List of sandwitch orders
+# List of sandwich orders
 
 sandwich_orders = ['chicken', 'grilled cheese', 'avocado', 'reuben', 'cuban', 'italian panini']
 finished_sandwiches = []
 
-# Loop through the sandwitch orders and print a message for each order.
+# Loop through the sandwich orders and print a message for each order.
 while sandwich_orders:
     current_order = sandwich_orders.pop(0) # So that the list of the orders does not change.
-    print(f"I made your {current_order} sandwitch.")
+    print(f"I made your {current_order} sandwich.")
     finished_sandwiches.append(current_order)
 
-# Print a message for each finished sandwitch.
-print("\nThe following sandwitches have been made:")
+# Print a message for each finished sandwich.
+print("\nThe following sandwiches have been made:")
 for made_sandwich in finished_sandwiches:
     print(f"- {made_sandwich}")
