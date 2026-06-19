c2b797b7e80294b04f97387935516bd6bf904385
Celestine Wangechi <wangechicelestine@gmail.com>
2026-06-19 15:52:12 +0300

    A fuction that displays a message about a favorite book

---
+++ functions/favorite_book.py
@@ -0,0 +1,5 @@
+def favorite_book(book_title):
+    """Display a message about a favorite book."""
+
+    print(f"One of my favorite books is {book_title.title()}.")
+favorite_book("rich dad, poor dad")
