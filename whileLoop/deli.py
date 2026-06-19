0ed3491f42c7521d30eb7f9604ea6b94c14f80dc
Celestine Wangechi <wangechicelestine@gmail.com>
2026-06-19 12:49:50 +0300

    (fix: spelling mistakes and make sure that pop doesn't change the order of the list

--- whileLoop/deli.py
+++ whileLoop/deli.py
@@ -1,15 +1,15 @@
 # List of sandwitch orders
 
-sandwitch_orders = ['chicken', 'grilled cheese', 'avocado', 'reuben', 'cuban', 'italian panini']
-finished_sandwitches = []
+sandwich_orders = ['chicken', 'grilled cheese', 'avocado', 'reuben', 'cuban', 'italian panini']
+finished_sandwiches = []
 
 # Loop through the sandwitch orders and print a message for each order.
-while sandwitch_orders:
-    sandwitch = sandwitch_orders.pop()
-    print(f"I made your {sandwitch} sandwitch.")
-    finished_sandwitches.append(sandwitch)
+while sandwich_orders:
+    current_order = sandwich_orders.pop(0) # So that the list of the orders does not change.
+    print(f"I made your {current_order} sandwitch.")
+    finished_sandwiches.append(current_order)
 
 # Print a message for each finished sandwitch.
 print("\nThe following sandwitches have been made:")
-for made_sandwich in finished_sandwitches:
+for made_sandwich in finished_sandwiches:
     print(f"- {made_sandwich}")
