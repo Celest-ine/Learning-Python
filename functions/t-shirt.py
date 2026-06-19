4ea49062908f9fb1454212d0bb33430d76be4b87
Celestine Wangechi <wangechicelestine@gmail.com>
2026-06-19 16:18:04 +0300

    A fuction that prints a sentence summarizing the size of a shirt and the phrase printed on it.

---
+++ functions/t-shirt.py
@@ -0,0 +1,6 @@
+def make_shirt(size, phrase):
+    """Print a sentence summarizing the size of a T-shirt and the phrase printed on it."""
+
+    print(f"Your T-shirt size is {size} with the phrase: '{phrase.title()}' printed on it.")
+make_shirt("medium", "born to be witty,forced to be locked in")
+make_shirt(size="large", phrase="good vibes only")
