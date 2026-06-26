ce76b304fd4b7ea073d9bb41836ec4626f0f0ba7
Celestine Wangechi <wangechicelestine@gmail.com>
2026-06-26 13:04:43 +0300

    fix: the dictionary assigment was calling the function, use update() method to add manufacture and model name to **kwargs

--- functions/cars.py
+++ functions/cars.py
@@ -1,9 +1,16 @@
 def car(manufacture, model_name, **kwargs):
     """A function that stores information about a car in a dictionary."""
 
-    car[manufacture] = manufacture
-    car[model_name] = model_name
-    return car
+    car_info = {
+        'manufacture': manufacture,
+        'model name': model_name,
+    }
 
-car_model = ('subaru', 'outback', color='blue', tow_package=True)
-car_model = ('kia', 'sportage', color='black', warrant=True)
+    car_info.update(kwargs)
+
+    return car_info
+
+car_model = car('subaru', 'outback', color='blue', tow_package=True)
+print(car_model)
+car_model = car('kia', 'sportage', color='black', warrant=True)
+print(car_model)
