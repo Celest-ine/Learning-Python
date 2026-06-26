31b5328f9057260f0321e191d0af335d8863c91e
Celestine Wangechi <wangechicelestine@gmail.com>
2026-06-26 14:17:51 +0300

    add a function to print the user info in a user friendly way

--- functions/user_profile.py
+++ functions/user_profile.py
@@ -1,8 +1,18 @@
 def build_profile(first, last, **user_info):
     """A user profile of with first name, last name and 3 oter key value pairs."""
+    
+    profile = {
+        'first_name': first,
+        'last_name': last,
+    }
 
-    user_info[first_name] = first
-    user_info[last_name] = last
-    return user_info
-user_profile = ('celestine', 'wangechi', 'age'=25, 'location'='nairobi', 'country'='kenya')
-print(user_profile)
+    profile.update(user_info)
+
+    return profile
+def print_user_info(user_profile):
+    for key, value in user_profile.items():
+        formatted_key = key.replace('_', ' ')
+        print(f'{formatted_key.title()}: {value}')
+
+user_profile = build_profile('celestine', 'wangechi', age=25, favorite_color='black', country='kenya')
+print_user_info(user_profile)
