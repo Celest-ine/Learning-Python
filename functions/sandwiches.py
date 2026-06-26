cc81ce4b195898ea3ad00b9f148011cbafe7b872
Celestine Wangechi <wangechicelestine@gmail.com>
2026-06-26 11:23:32 +0300

    A fuction that accepts a list of items that a user wants in their sandwich

---
+++ functions/sandwiches.py
@@ -0,0 +1,9 @@
+def make_sandwich(*items):
+    """Accept a list of items that a user wants in threir sandwich."""
+    print("Your sandwich will have the following items:")
+    for item in items:
+        print(f"- {item}")
+
+make_sandwich("peanut butter", "jam")
+make_sandwich("avocado")
+make_sandwich("ham", "tomatoes", "onions", "mayonaise")
