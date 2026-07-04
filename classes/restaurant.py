4e64feed29c2dd460781d434dd0736bdcac176e8
Celestine Wangechi <wangechicelestine@gmail.com>
2026-07-04 16:39:45 +0300

    Create an instance of a resturant

---
+++ classes/restaurant.py
@@ -0,0 +1,19 @@
+class Restaurant:
+    """Simulates an instance of a resturant."""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initiate restutant name and cuisine type attributes."""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        """Prints the description of the resturant."""
+        print(f"{self.restaurant_name} servers {self.cuisine_type} cuisine.")
+
+    def open_restaurant(self):
+        """Prints a message indicating that the resturant is open."""
+        print(f"{self.restaurant_name} is now open!")
+
+restaurant = Restaurant("Sarovar", "Italian")
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
