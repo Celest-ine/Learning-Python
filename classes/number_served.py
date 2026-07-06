a0f7a72fc195c6ce3356f8bf179e56db5520a439
Celestine Wangechi <wangechicelestine@gmail.com>
2026-07-06 11:31:59 +0300

    Add an attributer with a default value

---
+++ classes/number_served.py
@@ -0,0 +1,33 @@
+# Use the resturant class to add a attribute, number_served, with a default value of 0.
+class Restaurant:
+    """Simulates an instance of a resturant."""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initiate restutant name and cuisine type attributes."""
+        
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        """Prints the description of the resturant."""
+        
+        print(f"{self.restaurant_name} serves {self.cuisine_type} cuisine.")
+        print(f"{self.restaurant_name} has served {self.number_served} customers.")
+
+    def open_restaurant(self):
+        """Prints a message indicating that the resturant is open."""
+        
+        print(f"{self.restaurant_name} is now open!")
+
+restaurant = Restaurant("Sarovar", "Italian")
+
+restaurant_1 = Restaurant("Biriyani House", "Indian")
+
+restaurant_2 = Restaurant("Java cafe", "Continental")
+
+restaurant_1.describe_restaurant()
+restaurant_2.describe_restaurant()
+restaurant.describe_restaurant()
+
+restaurant.open_restaurant()
