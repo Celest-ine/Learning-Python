5337c7cc79a3433a3cf07305f14c2ed9eb05308a
Celestine Wangechi <wangechicelestine@gmail.com>
2026-07-06 11:39:13 +0300

    apply the method open_restaurant() to restaurant_1 and retaurant_2

--- classes/number_served.py
+++ classes/number_served.py
@@ -27,8 +27,11 @@ restaurant_1 = Restaurant("Biriyani House", "Indian")
 restaurant_2 = Restaurant("Java cafe", "Continental")
 
 restaurant_1.describe_restaurant()
+restaurant_1.open_restaurant()
 print()
+
 restaurant_2.describe_restaurant()
+restaurant_2.open_restaurant()
 print()
 restaurant.describe_restaurant()
 
