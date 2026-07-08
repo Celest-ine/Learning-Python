79a4a56c5bcae2f76b15b986a49f3818b64f9548
Celestine Wangechi <wangechicelestine@gmail.com>
2026-07-08 11:51:51 +0300

    move the IceCreamStand class to its own module

--- classes/number_served.py
+++ classes/number_served.py
@@ -36,26 +36,6 @@ class Restaurant:
         
         print(f"{self.restaurant_name} is now open!")
 
-class IceCreamStand(Restaurant):
-    """Represents aspects of a restaurant specific to an ice cream stand."""
-
-    def __init__(self, restaurant_name, cuisine_type="Ice Cream"):
-        """Initialize attributes of the parent class.
-         Then initialize attributes specific to an ice cream stand.
-         """
-        super().__init__(restaurant_name, cuisine_type)
-        self.flavors = []
-    
-    def display_flavors(self):
-        """Print the flavors available in the ice cream stand."""
-
-        if self.flavors:
-            print(f"{self.restaurant_name} offers the following flavors:")
-            for flavor in self.flavors:
-                print(f"- {flavor}")
-        else:
-            print(f"{self.restaurant_name} currently has no flavored ice cream available.")
-
 my_ice_cream_stand = IceCreamStand("Sweet Treats")
 my_ice_cream_stand.describe_restaurant()
 my_ice_cream_stand.open_restaurant()
