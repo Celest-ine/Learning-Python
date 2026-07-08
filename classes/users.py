3278396a4323a0c2afe75de63dacb329d4aacdd0
Celestine Wangechi <wangechicelestine@gmail.com>
2026-07-08 12:35:10 +0300

    move the Privilege and Admin class to admin.py

--- classes/users.py
+++ classes/users.py
@@ -29,40 +29,7 @@ class User:
     def greet_user(self):
         """Prints a personalized greeting to the user."""
         print(f"Hello, {self.first_name} {self.last_name}. Welcome to our platform!")
-
-
-class Privileges:
-    """Describes the privileges fo special users."""
-
-    def __init__(self, privileges=None):
-        """Initialise the attribute of the class."""
-
-        if privileges is None: # To avoid making default value mutable so that each object gets its own list
-            privileges = [
-                "can post",
-                "can delete post",
-                "can ban user"
-                "can add user",
-            ]
-
-        self.privileges = privileges
-
-    def show_privileges(self):
-        """Display the privileges of the admin user."""
-
-        print("Admin Privileges:")
-        for privilege in self.privileges:
-            print(f"- {privilege}")
-
-class Admin(User):
-    """Represent an admin user."""
-
-    def __init__(self, first_name, last_name, age, location, occupation):
-        """Initialize attributes of the parent class.
-        Then initialize attributes specific to an admin user.
-        """
-        super().__init__(first_name, last_name, age, location, occupation)
-        self.privileges = Privileges()
+              
     
 jane = User("Jane", "Doe", 28, "New York", "Software Engineer")
 mary = User("Mary", "Bling", 49, "Uganda", "Data Scientist")
