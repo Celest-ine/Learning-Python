d3e816a93f738b2240b8c413c7cab0e5c80b0159
Celestine Wangechi <wangechicelestine@gmail.com>
2026-07-08 14:14:25 +0300

    consistent naming of the method to the class

--- classes/dice.py
+++ classes/dice.py
@@ -10,17 +10,17 @@ class Die:
 
         self.sides = sides
     
-    def roll_dice(self):
+    def roll_die(self):
         """Print a random number between 1 and the number of sides a die has."""
         
         face_of_die = randint(1, self.sides)
         print(f"Your rolled a {face_of_die}")
 
 my_die_roll = Die()
-my_die_roll.roll_dice()
+my_die_roll.roll_die()
 
 ben_die_roll = Die(20)
-ben_die_roll.roll_dice()
+ben_die_roll.roll_die()
 
 sarah_die_roll = Die(10)
-sarah_die_roll.roll_dice()
+sarah_die_roll.roll_die()
